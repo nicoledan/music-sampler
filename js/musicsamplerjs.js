3d0c73c49b48cf1376bd8631386ad60dde560000
nicoledan <nicole_k_dan@Michael-B-Dans-MacBook-Pro.local>
2016-11-08 23:55:53 -0500

    first commit

---
+++ js/musicsamplerjs.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+function hideAll(){
+  $("#firstSong").hide();
+  $("#secondSong").hide();
+  $("#thirdSong").hide();
+  $("#fourthSong").hide();
+}
+
+hideAll();
+
+$(".covers").click(function(){
+  hideAll();
+  switch ($(this).attr("id")){
+    case "dangerouslyInLoveCover":
+      $("#firstSong").show();
+      break;
+    case "4cover":
+      $("#secondSong").show();
+      break;
+    case "beyonceCover":
+      $("#thirdSong").show();
+      break;
+    case "lemonadeCover":
+      $("#fourthSong").show();
+      break;
+    }
+});
+
+})
